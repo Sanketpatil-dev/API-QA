7177dcfea6cad5a934846a743a351361a6e13021
Sanketpatil-dev <119466096+Sanketpatil-dev@users.noreply.github.com>
2023-03-14 11:35:40 +0000

    Delete Apiproject directory

--- Apiproject/event/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class eventsserializer(serializers.ModelSerializer):
-    class Meta:
-        model=events
-        fields = "__all__"
